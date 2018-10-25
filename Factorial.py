0158c9d97f00a83ea176c28a52c733a23d879c8d
jayendra-patil33 <43951088+jayendra-patil33@users.noreply.github.com>
2018-10-25 19:24:33 +0530

    Add files via upload

---
+++ Factorial.py
@@ -0,0 +1,10 @@
+# Jayendra Patil 
+# roll no: 19, gr no: 11810706 
+# Factorial
+# question: 3
+num = int(input("enter a number: "))
+fac = 1
+for i in range(1, num + 1):
+    fac = fac * i
+print("factorial of ", num, " is ", fac)
+
