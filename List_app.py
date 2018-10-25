c691333f0ebbbea7eaf840a56358c36d126e558f
jayendra-patil33 <43951088+jayendra-patil33@users.noreply.github.com>
2018-10-25 19:47:17 +0530

    Add files via upload

---
+++ List_app.py
@@ -0,0 +1,24 @@
+# Jayendra Patil 
+# roll no: 19, gr no: 11810706 
+# Factorial
+# question: 3
+b=[]
+c=[]
+x=[]
+y=[]
+z=[]
+a=[]
+while 1: 
+ b.append(input("Enter your roll no"))
+ c.append(input("Enter your gr no"))
+ x.append(input("Enter your name:")) 
+ y.append(input("Enter your marks in Maths:")) 
+ z.append(input("Enter your marks in Science:")) 
+ a.append(input("Enter your marks in English:")) 
+ d=input("do you want to continue [y/n]")
+ if d=="y":
+     pass 
+ else:
+     break  
+print(b,c,x,y,z,a)
+ 
