0b827c6f6e104c61712bbd31a2fa6279cf8fecf8
jayendra-patil33 <43951088+jayendra-patil33@users.noreply.github.com>
2018-12-04 12:34:54 +0530

    Add files via upload

---
+++ Input1.py
@@ -0,0 +1,8 @@
+# Jayendra Patil 
+# roll no: 19, gr no: 11810706 
+# To take user input
+# question: 1
+Name=input("Enter your name ")
+Marks=int(input("Enter your marks "))
+print(" Your name is : "+Name)
+print(" Your marks are : "+str(Marks))
